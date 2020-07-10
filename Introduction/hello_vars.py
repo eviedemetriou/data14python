1cad1f2c0831648b41762a2ce50cec532ca83540
EvieDem <edemetriou@spartaglobal.com>
2020-07-10 12:36:02 +0100

    Created hello_vars

---
+++ Introduction/hello_vars.py
@@ -0,0 +1,16 @@
+#hw = "Hello world!"
+
+#name = input("Insert your name:  \n")
+#age = int(input("Insert your age:  "))
+#sibl = int(input("Insert the number of siblings you have:  "))
+#decNum = float(input("Insert your favourite decimal number:  "))
+#animal = input("Insert your favourite animal:  ")
+
+#print(f"{name}, {age}, {sibl}, {decNum}, {animal}")
+
+hw = "Hello World!"
+print(hw.isalpha())
+print(hw.islower())
+print(hw.endswith('!'))
+print(hw.startswith('H'))
+
