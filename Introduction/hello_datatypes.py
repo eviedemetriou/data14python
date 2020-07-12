7adb3c97fad39f59c491cd529b54bdafcf01fa0d
EvieDem <edemetriou@spartaglobal.com>
2020-07-12 21:36:30 +0100

    modified hello_datatypes

---
+++ Introduction/hello_datatypes.py
@@ -0,0 +1,36 @@
+# NUMERIC TYPES
+
+a = "hello"
+b = 3
+c = 4.9
+d = 4+12j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(c), "\n")
+
+# BOOLEANS
+
+hw = "Hello World!"
+print(hw.isalpha())  # alphabetic characters only
+print(hw.islower())
+print(hw.endswith('!'))
+print(hw.startswith('H'))
+print(type(True), type(False), "\n")
+
+x = None
+y = None
+print(x == y)
+print(x is y, "\n")
+
+print(True + True)
+print(True * True)
+print(True + False)
+print(True * False)
+print(False + False)
+print(False * False, "\n")
+
+# NONE TYPE
+
+print(type(None))
