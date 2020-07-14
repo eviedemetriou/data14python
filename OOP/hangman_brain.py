7d7348dff47a44775bac108fba6832f25ad80969
EvieDem <edemetriou@spartaglobal.com>
2020-07-14 21:55:09 +0100

    hangman game updates

--- OOP/hangman_brain.py
+++ OOP/hangman_brain.py
@@ -1,42 +1,88 @@
 from random import choice
+from hangman_game import Game
 
 word_list = ["anaconda", "supernova", "psychedelic", "autodidact"]
 
-class Brain:
+class Brain(Game):
 
-    def __init__(self):
+    def __init__(self, attempts=8):
+        #Game.__init__(self)
+        self.game = Game()
+        self.user_name = ''
+        self.attempts = attempts
         self.word = choice(word_list)
         self.word_length = len(self.word)
         self.first_letter = self.word[0]
         self.last_letter = self.word[-1]
-
-    def hangman_update(self, guess : str, initial):
-        index = []
-        for i in range(0, self.word_length):
-            if self.word[i] == guess:
-                index.append(i)
-        for j in index:
-            initial[j] = guess
-        print(initial)
+        self.initial_status = []
 
     def hangman_initiation(self):
-        initial = ["_"] * self.word_length
-        game_choice = input("Do you want the easy version (first and last letters given)? Y/N \n")
+        self.user_name = input("Hello user! What is your name? \n")
+        game_choice = input(f"{self.user_name} do you want the easy version (first and last letters given)? Y/N \n")
+        self.initial_status = ["_"] * self.word_length  # INITIALIZING hidden word
         if game_choice.upper() == 'Y' or game_choice.upper() == 'YES':
-            print("As you like!")
-            initial[0] = self.first_letter
-            initial[-1] = self.last_letter
-            print(initial)
-            for i in range(10):
-                guess = input("Make a guess:  \n")
-                self.hangman_update(guess, initial)
+            print("As you like!\n")
+            self.initial_status[0] = self.first_letter
+            self.initial_status[-1] = self.last_letter
+            print(self.initial_status)
         else:
-            print(initial)
-            for i in range(10):
-                guess = input("Make a guess:  \n")
-                self.hangman_update(guess, initial)
+            print("As you like!\n")
+            print(self.initial_status)
+        print(f"You can have {self.attempts} guesses. If you don't find the word you lose! \n")
+
+    def hangman_update(self):
+        index = []
+        for i in range(0, self.word_length):
+            if self.word[i] == self.game.guess:
+                index.append(i)
+        for j in index:
+            self.initial_status[j] = self.game.guess
+        print(self.initial_status)
+
+    def hangman_win(self):
+        word = []
+        for letter in self.word:
+            word.append(letter)
+        print(word)
+        print(f"Congratulations {self.user_name}! You won!!!")
+
+    def hangman_lose(self):
+        print(f"Sorry {self.user_name} but this is not the right word. You lost!")
 
 
 word1 = Brain()
-word1.hangman_initiation()
 
+word1.hangman_initiation()
+while word1.game.total_guesses < word1.attempts:
+    word1.game.make_guess()
+
+    if len(word1.game.guess) > 1:  # For case when user chooses to guess the whole word
+        if word1.game.guess == word1.word:
+            word1.hangman_win()
+        else:
+            word1.hangman_lose()
+        break
+
+    word1.hangman_update()
+    if '_' not in word1.initial_status:
+        print(f"Congratulations {word1.user_name}! You won!!!")
+        break
+    word1.game.update_guess_list()
+
+    if word1.game.total_guesses == word1.attempts - 1:  # Distinct statement for the final guess of the user
+        print("You only have ONE MORE guess.")
+        word1.game.make_guess()
+
+        if len(word1.game.guess) > 1:  # For case when user chooses to guess the whole word
+            if word1.game.guess == word1.word:
+                word1.hangman_win()
+            else:
+                word1.hangman_lose()
+            break
+
+        word1.hangman_update()
+        if '_' not in word1.initial_status:
+            print(f"Congratulations {word1.user_name}! You have won!!!")
+        else:
+            print(f"Sorry {word1.user_name}, you lost!")
+        break
--- OOP/hangman_game.py
+++ OOP/hangman_game.py
@@ -1,45 +1,23 @@
-from hangman_brain import Brain
 
-class Game(Brain):
+class Game():
 
     def __init__(self):
-        #Brain.__init__(self)
-        self.brain = Brain()
         self.guess = ''
         self.guesses_list = []
         self.total_guesses = 0
 
     def make_guess(self):
-        self.guess = input("Make a guess:  \n")
-        while self.guess in self.guesses_list:
-            self.guess = input(f"You have already used {self.guess}. Make another guess:  \n")
+        user_option = input("Do you want to guess a letter (type '1') or the word (type '2')? ")
+        if user_option == '1':
+            self.guess = input("Make a guess (letter): \n")
+            while self.guess in self.guesses_list:
+                self.guess = input(f"You have already used {self.guess}. Make another guess:\n")
+        else:
+            self.guess = input("Type the word you are thinking: \n")
 
     def update_guess_list(self):
         self.guesses_list.append(self.guess)
         self.total_guesses = len(self.guesses_list)
-        return self.guesses_list
-
-print("hello")
-word1 = Brain()
-game1 = Game()
-while game1.total_guesses < 5:
-    game1.make_guess()
-    game1.update_guess_list()
-    print(f"Your guesses so far are: {self.guesses_list}")
-
-
-# while game1.total_guesses < 5:
-#     guess = input("Make a guess:  \n")
-#     while guess in game1.guesses_list:
-#         guess = input(f"You have already used {guess}. Make another guess:  \n")
-#     print(game1.update_guess_list(guess))
-#     print(f"You made {game1.total_guesses} guess(es) so far!")
-#
-#     if game1.total_guesses == 4:
-#         guess = input("Make your final guess:  \n")
-#         while guess in game1.guesses_list:
-#             guess = input(f"You have already used {guess}. Make another guess:  \n")
-#
-#         game1.total_guesses = 5
-#         print("You have made the max number of guesses allowed.")
+        #print(f"You have made {self.total_guesses} guess.")
+        print(f"Your guesses so far are: {self.guesses_list}")
 
