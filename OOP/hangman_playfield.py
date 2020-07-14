b14d7c9ae19694663eea37d55e264610a0c63305
EvieDem <edemetriou@spartaglobal.com>
2020-07-14 22:35:01 +0100

    hangman update

--- OOP/hangman_playfield.py
+++ OOP/hangman_playfield.py
@@ -4,39 +4,40 @@ word1 = Brain()
 
 while word1.play_again == True:
 
-    word1.hangman_initiation()
+    word1.hangman_initiation()  # Initializing word to be presented to user
     while word1.game.total_guesses < word1.attempts:
-        word1.game.make_guess()
+        word1.game.make_guess()  # Make a guess
 
-        if len(word1.game.guess) > 1:  # Case when user chooses to guess the whole word
+        if len(word1.game.guess) > 1:  # Case when user chooses to guess the whole word - Check for win/loss
             if word1.game.guess == word1.word:
                 word1.hangman_win()
             else:
                 word1.hangman_lose()
             break
 
-        word1.hangman_update()  # UPDATE letters in word when applicable
-        if '_' not in word1.initial_status:  # Check for WIN
+        word1.hangman_update()  # UPDATE letters in word
+        if '_' not in word1.initial_status:  # Check for win
             print(f"Congratulations {word1.user_name}! You won!!!")
             break
         word1.game.update_guess_list()
 
-        if word1.game.total_guesses == word1.attempts - 1:  # Distinct statement for the final guess of the user
+        if word1.game.total_guesses == word1.attempts - 1:  # Distinct process for the FINAL GUESS of the user
             print("You only have ONE MORE guess.")
-            word1.game.make_guess()
+            word1.game.make_guess()  # Make a guess
 
-            if len(word1.game.guess) > 1:  # For case when user chooses to guess the whole word
+            if len(word1.game.guess) > 1:  # Case when user chooses to guess the whole word - Check for win/loss
                 if word1.game.guess == word1.word:
                     word1.hangman_win()
                 else:
                     word1.hangman_lose()
                 break
 
-            word1.hangman_update()
-            if '_' not in word1.initial_status:
+            word1.hangman_update()  # UPDATE word with letters
+            if '_' not in word1.initial_status:  # Check for win or loss
                 print(f"Congratulations {word1.user_name}! You have won!!!")
             else:
                 print(f"Sorry {word1.user_name}, you lost!")
             break
 
-    word1.hangman_renew()
+    word1.hangman_renew()  # Choice to play again (not working)
+
