3a2570edb02980defa3eca8afd0465eb7c0459f6
eviedemetriou <edemetriou@spartaglobal.com>
2020-07-21 21:25:29 +0100

    poke api update

--- HTTP_and_APIs/poke_ability_api.py
+++ HTTP_and_APIs/poke_ability_api.py
@@ -7,19 +7,12 @@ from pprint import pprint
 class PokeAbilities:
 
     def __init__(self, single_ability):
-        #self.abilities = abilities
         self.single_ability = single_ability
         self.ability_address = f"https://pokeapi.co/api/v2/ability/{self.single_ability}"
         self.ability_request = requests.get(self.ability_address)
         self.ability_response_json = self.ability_request.json()
         self.ability_generation_url = self.ability_response_json["generation"]["url"]
         self.generation = self.ability_response_json["generation"]["name"]
-        # self.poke_abilities = abilities
-        # self.json_body = json.dumps(self.poke_abilities)
-        # self.headers = {"Content-Type": "application/json"}
-        # self.address = "https://pokeapi.co/api/v2/ability/"
-        # self.request = requests.post(self.address, headers=self.headers, data=self.json_body)
-        # self.response_json = self.request.json()
 
     def show_ability_details(self):
         print("\n" + self.single_ability + " details:")
