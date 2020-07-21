246b2a65b14c2d2d3e15eda121bc3b2b32dea137
eviedemetriou <edemetriou@spartaglobal.com>
2020-07-21 23:32:22 +0100

    pokemon api update

--- HTTP_and_APIs/poke_ability_gener_api.py
+++ HTTP_and_APIs/poke_ability_gener_api.py
@@ -7,9 +7,9 @@ from poke_ability_api import PokeAbilities
 class PokeAbilityGeneration:
 
     def __init__(self, ability):
-        self.ability_obj = PokeAbilities(ability)
-        self.generation = self.ability_obj.ability_response_json["generation"]["name"]
-        self.generation_url = self.ability_obj.ability_response_json["generation"]["url"]
+        ability_obj = PokeAbilities(ability)
+        self.generation = ability_obj.ability_response_json["generation"]["name"]
+        self.generation_url = ability_obj.ability_response_json["generation"]["url"]
         self.generation_address = f"https://pokeapi.co/api/v2/generation/{self.generation_url}"
         self.generation_request = requests.get(self.generation_address)
         self.generation_response_json = self.generation_request.json()
